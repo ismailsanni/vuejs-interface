d04f4ff7b3452367975010cd7deb8c5885637e1a
Ismail Sanni <sanni920@gmail.com>
2020-03-09 14:37:41 +0100

    add search feature

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="main-app" class="container">
     <div class="row justify-content-center">
+      <search-appointment
+        @searchRecords="searchAppointment"
+        :myKey="filterKey"
+        :myDir="filterDir"
+        @requestKey="changeKey"
+        @requestDir="changeDir"
+      />
       <add-appointment @add="addItem" />
       <appointment-list
-        :appointments="appointments"
+        :appointments="filteredApts"
         @remove="removeItem"
         @edit="editItem"
       />
@@ -14,6 +21,8 @@
 <script>
 import AppointmentList from "./components/AppointmentList";
 import AddAppointment from "./components/AddAppointment";
+import SearchAppointment from "./components/SearchAppointment";
+
 import axios from "axios";
 import _ from "lodash";
 
@@ -23,7 +32,10 @@ export default {
     return {
       title: "Appointment List",
       appointments: [],
-      aptIndex: 0
+      aptIndex: 0,
+      searchTerms: "",
+      filterKey: "petName",
+      filterDir: "asc"
     };
   },
   mounted() {
@@ -38,7 +50,28 @@ export default {
   },
   components: {
     AppointmentList,
-    AddAppointment
+    AddAppointment,
+    SearchAppointment
+  },
+  computed: {
+    searchApts: function() {
+      return this.appointments.filter(item => {
+        return (
+          item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
+        );
+      });
+    },
+    filteredApts: function() {
+      return _.orderBy(
+        this.searchApts,
+        item => {
+          return item[this.filterKey].toLowerCase();
+        },
+        this.filterDir
+      );
+    }
   },
   methods: {
     removeItem: function(apt) {
@@ -54,6 +87,15 @@ export default {
       apt.aptId = this.aptIndex;
       this.aptIndex++;
       this.appointments.push(apt);
+    },
+    searchAppointment: function(terms) {
+      this.searchTerms = terms;
+    },
+    changeKey: function(key) {
+      this.filterKey = key;
+    },
+    changeDir: function(dir) {
+      this.filterDir = dir;
     }
   }
 };
